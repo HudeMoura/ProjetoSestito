9d8e471bd118eb6b6410cb9a66140ae5025d63f5
HudeMoura <felipemoura92@gmail.com>
2024-03-06 19:15:04 -0300

    Tentativa Final

--- TrocaDeTela/script.js
+++ TrocaDeTela/script.js
@@ -1,7 +1,16 @@
-function trocar() {
-    document.getElementById('active').src="home";
-}
+var imagemAtual = 1;
 
-function trocar2() {
-    document.getElementById('active').src="work";
+    function trocarImagem() {
+        var imagem1 = document.getElementById('imagem1');
+        var imagem2 = document.getElementById('imagem2');
+
+    if (imagemAtual === 1) {
+        imagem1.style.opacity = '0';
+        imagem2.style.opacity = '1';
+        imagemAtual = 2;
+    } else {
+        imagem1.style.opacity = '1';
+        imagem2.style.opacity = '0';
+        imagemAtual = 3;
+    }
 }
